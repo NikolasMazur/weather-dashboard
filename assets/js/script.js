614e1e219847911ba8a1f0fca0e3205a78c52e92
Nikolas <kolemazur@gmail.com>
2022-04-01 21:57:28 -0600

    Add query selector variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-// Placeholder
+// Query Selector Variables
+var currentDate = moment();
+var searchInput = document.querySelector(".user-input");
+var inputValue = $("#user-input");
+var searchBtn = document.querySelector(".search-button");
+var recentContainer = $("#recent");
+var cityDate = document.querySelector(".city-date-icon");
+var weather = document.querySelector(".current-weather");
+var temp = document.querySelector(".temp");
+var humidity = document.querySelector(".humidity");
+var wind = document.querySelector(".wind");
+var uv = document.querySelector(".uv-index");

78069559f197c32ebf074e005554f6e53dd2e81f
Nikolas <kolemazur@gmail.com>
2022-04-02 23:04:11 -0600

    Add asynchronous event listener/API fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Adds query selectors
 var currentDate = moment();
 var searchInput = document.querySelector(".user-input");
 var searchBtn = document.querySelector(".search-button");
@@ -22,3 +23,42 @@ var searchSubmitHandler = function (event) {
   }
 };
 
+searchBtn.addEventListener("click", async function () {
+    // Adds API URL/key
+    var apiUrl =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      searchInput.value +
+      "&units=imperial&appid=9795009f60d5d1c3afe4e6df6002c319";
+    // API fetch
+    var response = await fetch(apiUrl);
+      console.log(response);
+        if (response.ok) {
+          console.log(response);
+          var data = await response.json();
+            var nameValue = data.name;
+            var tempValue = data.main.temp;
+            var humidityValue = data.main.humidity;
+            var windValue = data.wind.speed;
+            console.log(data);
+            var lat = data.coord.lon;
+            var lon = data.coord.lat;
+            await uvIndex(data.coord.lat, data.coord.lon);
+            var icon = data.weather[0].icon;
+
+            var weatherURL =`http://openweathermap.org/img/wn/${icon}.png`;
+            var icon = `<img src="${weatherURL}"/>`;
+           console.log(weatherIcon);
+  
+            cityDateIcon.innerHTML = 
+              nameValue + currentDate.format(" (M/DD/YYYY) ") + icon;
+            temp.innerHTML = "Temperature: " + tempValue + " °F";
+            humidity.innerHTML = "Humidity: " + humidityValue + "%";
+            wind.innerHTML = "Wind Speed: " + windValue + " MPH";
+            console.log(weatherIcon);
+              
+              console.log(icon);
+  
+        } else {
+          alert("Error: " + response.statusText);
+        }
+});
